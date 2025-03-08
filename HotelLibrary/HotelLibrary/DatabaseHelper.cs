07e327281801fd4d731787a7df25565f0f59d53c
Sujal-Bangari <167900534+Sujal-Bangari@users.noreply.github.com>
2025-03-08 02:03:12 -0800

    Add files via upload

---
+++ HotelLibrary/HotelLibrary/DatabaseHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HotelLibrary
+{
+    public class DatabaseHelper
+    {
+        private string conn = "Server=WIN2019;Database=HotelAppDb;Trusted_Connection=True;";
+        public void ExecuteQuery(string query)
+        {
+            using(SqlConnection sc=new SqlConnection(conn))
+            {
+                sc.Open();
+                using(SqlCommand scmd=new SqlCommand(query, sc))
+                {
+                    scmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public DataTable GetData(string query)
+        {
+            using (SqlConnection sc = new SqlConnection(conn))
+            {
+                sc.Open();
+                using (SqlCommand scmd = new SqlCommand(query, sc))
+                {
+                    using(SqlDataAdapter da=new SqlDataAdapter(scmd))
+                    {
+                        DataTable dt = new DataTable();
+                        da.Fill(dt);
+                        return dt;
+                    }
+                }
+            }
+        }
+    }
+}
